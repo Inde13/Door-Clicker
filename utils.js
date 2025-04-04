68a6c93b671d22304796ea7c06ba5349a85ba375
Inde13 <96017282+Inde13@users.noreply.github.com>
2025-04-04 18:52:39 -0300

    Code fixing test

--- utils.js
+++ utils.js
@@ -1,8 +1,8 @@
 function formatNum(x) {
-								power = Math.log10(x)
-								mantissa = x / power
-								if (power < 6) {
-																return x.toFixed(2)
-								}
-								return `${mantissa.toFixed(2)}e${power}`
+	power = Math.log10(x)
+	mantissa = x / power
+	if (power < 6) {
+		return x.toFixed(2)
+	}
+	return `${mantissa.toFixed(2)}e${power}`
 }
